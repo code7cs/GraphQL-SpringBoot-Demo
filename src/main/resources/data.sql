96ae90366016b5ce657436ba01a8de1a3b9e8516
hanfanw <hanfanw@gmail.com>
2020-08-31 13:28:30 -0400

    2

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,2 @@
-INSERT INTO books(name,pages) VALUES('GraphQL Book', 1000);
-INSERT INTO books(name,pages) VALUES('SpringBoot Book', 2000);
+INSERT INTO book(name,pages) VALUES('GraphQL Book', 1000);
+INSERT INTO book(name,pages) VALUES('SpringBoot Book', 2000);
